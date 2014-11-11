f390b7102380ade7538825ff94a3202c9c3bf635
Deadstar117 <stephane.pelletierlamothe@gmail.com>
2014-11-11 16:55:35 -0500

    Ajout de deux constructeur.

--- PathfinderCharacterSheet/PathfinderCharacterSheet/Character/Statistics/Ability.cs
+++ PathfinderCharacterSheet/PathfinderCharacterSheet/Character/Statistics/Ability.cs
@@ -8,5 +8,26 @@ namespace PathfinderCharacterSheet.Character.Statistics
 {
     class Ability
     {
+        private int score;
+        private int modifier;
+        private int tempAdjustment;
+        private int tempModifier;
+
+        public Ability(int abilityScore)
+        {
+            this.score = abilityScore;
+            this.tempAdjustment = 0;
+        }
+
+        public Ability(int abilityScore, int temporaryAdjustment)
+        {
+            this.score = abilityScore;
+            this.tempAdjustment = temporaryAdjustment;
+        }
+
+        private void calculateStats()
+        {
+
+        }
     }
 }
