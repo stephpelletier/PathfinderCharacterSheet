3b48b21d694090503642c4cc5acf35431b664f6a
Deadstar117 <stephane.pelletierlamothe@gmail.com>
2014-11-11 19:04:45 -0500

    Ajout de la fonction 'calculateStats'.

--- PathfinderCharacterSheet/PathfinderCharacterSheet/Character/Statistics/Ability.cs
+++ PathfinderCharacterSheet/PathfinderCharacterSheet/Character/Statistics/Ability.cs
@@ -17,17 +17,23 @@ namespace PathfinderCharacterSheet.Character.Statistics
         {
             this.score = abilityScore;
             this.tempAdjustment = 0;
+
+            calculateStats();
         }
 
         public Ability(int abilityScore, int temporaryAdjustment)
         {
             this.score = abilityScore;
             this.tempAdjustment = temporaryAdjustment;
+
+            calculateStats();
         }
 
         private void calculateStats()
         {
-
+            double tempScore = score - 10;
+            modifier = (int)Math.Floor(tempScore/2);
+            tempModifier = (int)Math.Floor((tempScore + tempModifier)/2);
         }
     }
 }
