1589e720163dc395a1fc3b8bca5cc141e0965ed0
Deadstar117 <stephane.pelletierlamothe@gmail.com>
2014-11-11 20:01:35 -0500

    Ajout d'un DTO pour transferer les stats a l'interface

--- PathfinderCharacterSheet/PathfinderCharacterSheet/Character/Statistics/StatsDTO.cs
+++ PathfinderCharacterSheet/PathfinderCharacterSheet/Character/Statistics/StatsDTO.cs
@@ -4,9 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PathfinderCharacterSheet.Character.Statistics
+namespace PathfinderCharacterSheet
 {
     struct StatsDTO
     {
+        public int strenghtScore;
+        public int strenghtModifier;
+        public int strenghtTempAdjustment;
+        public int strenghtTempModifier;
+
+        public int dexterityScore;
+        public int dexterityModifier;
+        public int dexterityTempAdjustment;
+        public int dexterityTempModifier;
+
+        public int constitutionScore;
+        public int constitutionModifier;
+        public int constitutionTempAdjustment;
+        public int constitutionTempModifier;
+
+        public int intelligenceScore;
+        public int intelligenceModifier;
+        public int intelligenceTempAdjustment;
+        public int intelligenceTempModifier;
+
+        public int wisdomScore;
+        public int wisdomModifier;
+        public int wisdomTempAdjustment;
+        public int wisdomTempModifier;
+
+        public int charismaScore;
+        public int charismaModifier;
+        public int charismaTempAdjustment;
+        public int charismaTempModifier;
     }
 }
