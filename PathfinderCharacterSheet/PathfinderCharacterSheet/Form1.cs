23dffbfd3920f2b280fce8773f158845a4a93973
Deadstar117 <stephane.pelletierlamothe@gmail.com>
2014-11-14 17:32:04 -0500

    Connection entre le DTO des stats et les textbox.

--- PathfinderCharacterSheet/PathfinderCharacterSheet/Form1.cs
+++ PathfinderCharacterSheet/PathfinderCharacterSheet/Form1.cs
@@ -21,20 +21,37 @@ namespace PathfinderCharacterSheet
         {
             Character character = new Character();
 
-            List<int> characterStats = character.getStats();
-            txtStrengthScore.Text = characterStats[0].ToString();
-            txtDexterityScore.Text = characterStats[1].ToString();
-            txtConstitutionScore.Text = characterStats[2].ToString();
-            txtIntelligenceScore.Text = characterStats[3].ToString();
-            txtWisdomScore.Text = characterStats[4].ToString();
-            txtCharismaScore.Text = characterStats[5].ToString();
+            StatsDTO characterStats = character.getStats();
 
-            txtStrengthTempAdjustment.Text = characterStats[6].ToString();
-            txtDexterityTempAdjustment.Text = characterStats[7].ToString();
-            txtConstitutionTempAdjustment.Text = characterStats[8].ToString();
-            txtIntelligenceTempAdjustment.Text = characterStats[9].ToString();
-            txtWisdomTempAdjustment.Text = characterStats[10].ToString();
-            txtCharismaTempAdjustment.Text = characterStats[11].ToString();
+            txtStrengthScore.Text = characterStats.strenghtScore.ToString();
+            txtStrengthModifier.Text = characterStats.strenghtModifier.ToString();
+            txtStrengthTempAdjustment.Text = characterStats.strenghtTempAdjustment.ToString();
+            txtStrengthTempModifier.Text = characterStats.intelligenceTempModifier.ToString();
+
+            txtDexterityScore.Text = characterStats.dexterityScore.ToString();
+            txtDexterityModifier.Text = characterStats.dexterityModifier.ToString();
+            txtDexterityTempAdjustment.Text = characterStats.dexterityTempAdjustment.ToString();
+            txtDexterityTempModifier.Text = characterStats.dexterityTempModifier.ToString();
+
+            txtConstitutionScore.Text = characterStats.constitutionScore.ToString();
+            txtConstitutionModifier.Text = characterStats.constitutionModifier.ToString();
+            txtConstitutionTempAdjustment.Text = characterStats.constitutionTempAdjustment.ToString();
+            txtConstitutionTempModifier.Text = characterStats.constitutionTempModifier.ToString();
+
+            txtIntelligenceScore.Text = characterStats.intelligenceScore.ToString();
+            txtIntelligenceModifier.Text = characterStats.intelligenceModifier.ToString();
+            txtIntelligenceTempAdjustment.Text = characterStats.intelligenceTempAdjustment.ToString();
+            txtIntelligenceTempModifier.Text = characterStats.intelligenceTempModifier.ToString();
+
+            txtWisdomScore.Text = characterStats.wisdomScore.ToString();
+            txtWisdomModifier.Text = characterStats.wisdomModifier.ToString();
+            txtWisdomTempAdjustment.Text = characterStats.wisdomTempAdjustment.ToString();
+            txtWisdomTempModifier.Text = characterStats.wisdomTempModifier.ToString();
+
+            txtCharismaScore.Text = characterStats.charismaScore.ToString();
+            txtCharismaModifier.Text = characterStats.charismaModifier.ToString();
+            txtCharismaTempAdjustment.Text = characterStats.charismaTempAdjustment.ToString();
+            txtCharismaTempModifier.Text = characterStats.charismaTempModifier.ToString();
         }
 
         private void quitToolStripMenuItem_Click(object sender, EventArgs e)
